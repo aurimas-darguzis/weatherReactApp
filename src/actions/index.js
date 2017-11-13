cd21025444308f3c7aa5b6267f3a457af25987d4
Aurimas <aurimas@darguzis.com>
2017-11-13 09:24:30 +0000

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
-const API_KEY =  '3e63086947a52b6ff4ae8569298ce8e9'
+const API_KEY =  '3e63086947a52b6ff4ae8569298ce8e9';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather () {
+    return {
+        type: FETCH_WEATHER
+    };
+}
